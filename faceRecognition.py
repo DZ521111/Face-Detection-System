ae0a163a2b6d2159f3f620095109e7ee7aecb668
DZ521111 <dhruvkakadiya3@gmail.com>
2021-01-04 23:23:37 +0530

    face code updated!

--- faceRecognition.py
+++ faceRecognition.py
@@ -10,6 +10,7 @@ import cv2
 import os
 import numpy as np
 
+# function for face detection to detect the faces from laptop camera as of now
 def faceDetect(test_img):
     gray_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
     face_cascade = cv2.CascadeClassifier('C:/Users/DHRUV/Desktop/ML projects/Face Recognition System/haar_cascade/haarcascade_frontalface_default.xml') 
